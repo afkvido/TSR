50253bb77e499bc93e2c508afca0de4b8298b1ba
gemsvido <gemsvido@gmail.com>
2022-10-12 13:51:59 -0700

    progress

--- src/main/kotlin/afkvido/Main.kt
+++ src/main/kotlin/afkvido/Main.kt
@@ -14,7 +14,27 @@ fun main(args: Array<String>) {
 
     app.ws ("/") { ws ->
 
-        ws.onConnect {
+        ws.onConnect { connection ->
+
+            val ip : String = connection.session.remoteAddress.toString();
+            val isAlt = if (Config.altDetection) true /* TODO fix */ else false;
+            val isBanned = Userbase.get(ip) == KnownIP.BLACKLIST;
+            val isAdmin = Userbase.get(ip) == KnownIP.ADMINISTRATOR;
+
+            if (isBanned) {
+                connection.send("blacklisted-ip-kick");
+                connection.closeSession();
+            }
+            else if (isAlt && isAdmin) {
+
+            }
+            else if (isAlt) {
+                connection.send("alt-kick");
+                connection.closeSession();
+            }
+
+
+
 
         }
 
